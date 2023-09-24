d2bd6423e676296c150e0d2bb1278460e5f28603
Hamed Kamrava <h.kamrava1@gmail.com>
2023-09-24 13:09:21 +0100

    add test activity

---
+++ app/src/main/java/io/nekohasekai/sagernet/ui/TestActivity.kt
@@ -0,0 +1,12 @@
+package io.nekohasekai.sagernet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.nekohasekai.sagernet.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_test)
+    }
+}
